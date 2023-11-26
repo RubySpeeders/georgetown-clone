b5cd6be756c169e2e7253fae02d114f4ca64358b
Sarah Peters <sarahnpeters@gmail.com>
2023-11-26 13:54:17 -0500

    adding more styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,92 +57,130 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <div
-        style={{
-          position: "relative",
-          width: "500px",
-          height: "5rem",
-          background: "#041E42",
-        }}
-      >
-        <Image
-          src="/georgetown-title.png"
-          alt="georgetown-title"
-          //TODO FIGURE OUT HEIGHT AND WIDTH
-          layout="fill"
-          objectFit="contain"
-          objectPosition="center"
-        />
-      </div>
-      <h1>Hoyas in August Event Attendees</h1>
-      <div className={styles.flexContainer}>
-        <div className={styles.filtes}>
-          <h5>Filter Attendees</h5>
-          <Searchbar
-            label="By Name"
-            attendees={attendees}
-            onSearch={handleSearch}
-          />
-          <CheckboxContainer
-            label="School"
-            attendees={attendees}
-            options={["College", "Law"]}
-            onCheckboxChange={handleCheckboxChange}
-          />
-          <CheckboxContainer
-            label="Registration Status"
-            attendees={attendees}
-            options={["Registered", "Not Registered"]}
-            onCheckboxChange={handleCheckboxChange}
-          />
-          <CheckboxContainer
-            label="Class"
-            attendees={attendees}
-            options={["'68", "'72", "'86", "'92"]}
-            onCheckboxChange={handleCheckboxChange}
-          />
+    <>
+      <main>
+        <div>
+          <div
+            style={{
+              border: "1px red solid",
+              width: "100%",
+              height: "5rem",
+              background: "#041E42",
+            }}
+          ></div>
+          <div
+            style={{
+              border: "1px orange solid",
+              width: "33%",
+              height: "5rem",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              zIndex: 1,
+            }}
+          >
+            <Image
+              src="/georgetown-title.png"
+              alt="georgetown-title"
+              //TODO FIGURE OUT HEIGHT AND WIDTH
+              layout="fill"
+              objectFit="contain"
+              objectPosition="center"
+              style={{}}
+            />
+          </div>
         </div>
-        <div
-          style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}
-          className={styles.attendees}
-        >
-          {filteredAttendees.map((attendee) => (
-            <div
-              key={attendee.first_name}
-              style={{
-                border: "2px solid #333",
-                padding: "10px",
-                flex: "0 0 calc(50% - 10px)",
-                boxSizing: "border-box",
-              }}
-            >
-              <p>{attendee.school_year_code}</p>
-              <p>
-                {attendee.first_name} {attendee.last_name}
-              </p>
-              <p>{attendee.registered}</p>
-            </div>
-          ))}
+        <h1>Hoyas in August Event Attendees</h1>
+        <div className={styles.flexContainer}>
+          <div className={styles.filtes}>
+            <h5>Filter Attendees</h5>
+            <Searchbar
+              label="By Name"
+              attendees={attendees}
+              onSearch={handleSearch}
+            />
+            <CheckboxContainer
+              label="School"
+              attendees={attendees}
+              options={["College", "Law"]}
+              onCheckboxChange={handleCheckboxChange}
+            />
+            <CheckboxContainer
+              label="Registration Status"
+              attendees={attendees}
+              options={["Registered", "Not Registered"]}
+              onCheckboxChange={handleCheckboxChange}
+            />
+            <CheckboxContainer
+              label="Class"
+              attendees={attendees}
+              options={["'68", "'72", "'86", "'92"]}
+              onCheckboxChange={handleCheckboxChange}
+            />
+          </div>
+          <div
+            style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}
+            className={styles.attendees}
+          >
+            {filteredAttendees.map((attendee) => (
+              <div
+                key={attendee.first_name}
+                style={{
+                  border: "2px solid #041E42",
+                  padding: "10px",
+                  flex: "0 0 calc(50% - 10px)",
+                  boxSizing: "border-box",
+                }}
+              >
+                <p>{attendee.school_year_code}</p>
+                <p>
+                  {attendee.first_name} {attendee.last_name}
+                </p>
+                <p>{attendee.registered}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div
-        style={{
-          position: "relative",
-          width: "500px",
-          height: "5rem",
-          background: "#041E42",
-        }}
-      >
-        <Image
-          src="/georgetown-advancement-title.png"
-          alt="georgetown-advancement-title"
-          //TODO FIGURE OUT HEIGHT AND WIDTH
-          layout="fill"
-          objectFit="contain"
-          objectPosition="center"
-        />
-      </div>
-    </main>
+        <div>
+          <div
+            style={{
+              position: "relative",
+              border: "1px red solid",
+              width: "100%",
+              height: "5rem",
+              background: "#041E42",
+            }}
+          ></div>
+          <div
+            style={{
+              border: "1px orange solid",
+              width: "33%",
+              height: "5rem",
+              position: "absolute",
+              top: "65%",
+              right: 0,
+              zIndex: 1,
+              transform: "translate(0, -50%)",
+            }}
+          >
+            <Image
+              src="/georgetown-advancement-title.png"
+              alt="georgetown-advancement-title"
+              //TODO FIGURE OUT HEIGHT AND WIDTH
+              layout="fill"
+              objectFit="contain"
+              objectPosition="center"
+              style={{}}
+            />
+          </div>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <p>Contact</p>
+        <p>Accessibility</p>
+        <p>Copyright</p>
+        <p>Privacy</p>
+      </footer>
+    </>
   );
 }
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,14 +1,14 @@
-import { Attendee } from '@/app/page';
-import React, { useState } from 'react'
+import { Attendee } from "@/app/page";
+import React, { useState } from "react";
 
 interface Props {
-    label: string;
-    attendees: Attendee[];
-    onSearch: (filteredAttendees: Attendee[]) => void;
-  }
+  label: string;
+  attendees: Attendee[];
+  onSearch: (filteredAttendees: Attendee[]) => void;
+}
 
 const Searchbar = ({ label, attendees, onSearch }: Props) => {
-    const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.target.value;
@@ -28,20 +28,26 @@ const Searchbar = ({ label, attendees, onSearch }: Props) => {
     <div
       style={{
         display: "flex",
-        flexDirection: "column"
+        flexDirection: "column",
       }}
     >
       <label>{label}</label>
-      <div>
+      <div style={{ display: "flex" }}>
         <input
           type="text"
           value={searchValue}
           onChange={handleInputChange}
-          placeholder="Enter name..."
+          placeholder=""
+          style={{
+            border: "1px solid #63666A",
+            width: "100%",
+            height: "50px",
+            // background: `transparent url("../images/icons/icon-search-blue.svg") no-repeat`,
+          }}
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
